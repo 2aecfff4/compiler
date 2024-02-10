e77271021abd0d210567b95aad4ff9ac7e46867a
2aecfff4 <11327470+2aecfff4@users.noreply.github.com>
2024-02-10 14:21:43 +0100

    Remove unused code from `Labels`

--- crates/ir/src/label.rs
+++ crates/ir/src/label.rs
@@ -67,39 +67,12 @@ impl Labels {
         self.labels.iter()
     }
 
-    /// Retrieves the target labels associated with the last instruction of a specific label.
-    /// This method assumes that the label exists in the instruction map.
     ///
-    /// Returns a [`SmallVec`] containing the target labels or panics if the label or targets are not present.
-    fn targets(&self, label: Label) -> SmallVec<[Label; 8]> {
-        self.get(label)
-            .instructions
-            .last()
-            .unwrap()
-            .targets()
-            .unwrap()
     pub fn iter_mut(&mut self) -> impl Iterator<Item = (&Label, &mut LabelData)> {
         self.labels.iter_mut()
     }
 
     ///
-    pub fn traverse_bfs(&self, start: Option<Label>, mut callback: impl FnMut(Label)) {
-        let num_labels = self.labels.len();
-        let mut visited = HashSet::with_capacity(num_labels);
-        let mut queue = VecDeque::with_capacity(num_labels / 2);
-
-        let start = start.unwrap_or_else(|| Label(0));
-        queue.push_back(start);
-
-        while let Some(label) = queue.pop_front() {
-            if !visited.insert(label) {
-                continue;
-            }
-
-            callback(label);
-            for target in self.targets(label).iter() {
-                queue.push_back(*target);
-            }
     pub fn labels(&self) -> impl Iterator<Item = Label> {
         let count = self.labels.len() as u32;
         (0..count).map(Label)
